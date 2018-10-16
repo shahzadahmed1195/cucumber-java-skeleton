dcf9fb2f32d584d85ee66ea664050835dc668a0c
shahzadahmed1195 <44030615+shahzadahmed1195@users.noreply.github.com>
2018-10-16 12:52:55 -0400

    delete step defs

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -8,15 +8,4 @@ public class Stepdefs {
         Belly belly = new Belly();
         belly.eat(cukes);
     }
-    @When("I wait {int} hour")
-public void i_wait_hour(Integer int1) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new cucumber.api.PendingException();
-    }
-
-@Then("my belly should growl")
-public void my_belly_should_growl() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new cucumber.api.PendingException();
-    }
 }
